43966af3feb57408e6538a0d91e0075901d82984
Danny Peck <dannytpeck@gmail.com>
2015-01-24 15:04:50 -0700

    Added more constants from room.py

--- constants.py
+++ constants.py
@@ -8,18 +8,22 @@
 
 FPS = 60 # frames per second to update the DISPLAYSURF
 
-TOTAL_LEVEL_WIDTH = 1600
-TOTAL_LEVEL_HEIGHT = 1600
 WINWIDTH = 800 # width of the program's window, in pixels
 WINHEIGHT = 800 # height in pixels
-HALF_WINWIDTH = int(WINWIDTH / 2)
-HALF_WINHEIGHT = int(WINHEIGHT / 2)
+HALF_WINWIDTH = WINWIDTH // 2
+HALF_WINHEIGHT = WINHEIGHT // 2
 
-ROWS = 25
-COLUMNS = 25
 TILESIZE = 64
-LONG_STRING = "X" * 50
+
+# LONG_STRING = "X" * 50
 
 # Colors
 BLACK   = (   0,   0,   0)
 WHITE   = ( 255, 255, 255)
+RED     = ( 255,   0,   0)
+GREEN   = (   0, 255,   0)
+BLUE    = (   0,   0, 255)
+CYAN    = (   0, 255, 255)
+MAGENTA = ( 255,   0, 255)
+YELLOW  = ( 255, 255,   0)
+CRIMSON = (  35,   0,   0)
