3d8b7fa3abe4acf4193a63c9c12d8207e5ad2f45
Danny Peck <dannytpeck@gmail.com>
2015-01-23 17:21:06 -0700

    Added
    Moving all the constants to their own file.

---
+++ constants.py
@@ -0,0 +1,25 @@
+"""
+   Scythe's Secrets (Working Title)
+   By Danny Peck dannytpeck@gmail.com
+   https://github.com/dannytpeck/scythes-secrets
+
+   Constants File
+"""
+
+FPS = 60 # frames per second to update the DISPLAYSURF
+
+TOTAL_LEVEL_WIDTH = 1600
+TOTAL_LEVEL_HEIGHT = 1600
+WINWIDTH = 800 # width of the program's window, in pixels
+WINHEIGHT = 800 # height in pixels
+HALF_WINWIDTH = int(WINWIDTH / 2)
+HALF_WINHEIGHT = int(WINHEIGHT / 2)
+
+ROWS = 25
+COLUMNS = 25
+TILESIZE = 64
+LONG_STRING = "X" * 50
+
+# Colors
+BLACK   = (   0,   0,   0)
+WHITE   = ( 255, 255, 255)
