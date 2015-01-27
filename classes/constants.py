16e44486b3bc726ef0d033151b85c2f8a4c1746d
Danny Peck <dannytpeck@gmail.com>
2015-01-27 00:03:30 -0700

    Constants file

---
+++ classes/constants.py
@@ -0,0 +1,24 @@
+"""
+   Scythe's Secrets (Working Title)
+   By Danny Peck dannytpeck@gmail.com
+   https://github.com/dannytpeck/scythes-secrets
+
+   Constants File
+"""
+
+RESOURCES_DIR = 'data'
+PLAYER_MOVE_SPEED = 200 # pixels per second
+MAP_FILENAME = 'hedgemaze.tmx'
+
+# Colors
+BLACK = (   0,   0,   0)
+WHITE = ( 255, 255, 255)
+RED = ( 255,   0,   0)
+GREEN = (   0, 255,   0)
+BLUE = (   0,   0, 255)
+CYAN = (   0, 255, 255)
+MAGENTA = ( 255,   0, 255)
+YELLOW = ( 255, 255,   0)
+CRIMSON = (  35,   0,   0)
+GRAY = (  51,  51,  51)
+PALEYELLOW  = ( 255, 255, 153)
